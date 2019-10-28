b907c6c8adadad99d126d0d97258dfccd07c79d8
Josh Borrow <joshua.borrow@durham.ac.uk>
2019-10-28 13:16:18 +0000

    Messed around with the unyt library to enable msun to be a first-class unit

--- velociraptor/__init__.py
+++ velociraptor/__init__.py
@@ -1,3 +1,19 @@
+"""
+The velociraptor module. 
+
+More information is available in the documnetation.
+"""
+
+# First things first, we need to upgrade msun from a symbol to a
+# first-class unit.
+import unyt
+
+try:
+    unyt.define_unit("msun", unyt.msun, rex_repr=r"M_\odot")
+except RuntimeError:
+    # We've already done that, oops.
+    pass
+
 from velociraptor.catalogue.catalogue import generate_catalogue
 
 
