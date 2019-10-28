b7656a3074025151aeb3a065905bc3e4cf494d95
Josh Borrow <joshua.borrow@durham.ac.uk>
2019-10-28 16:06:03 +0000

    Added function to create a mass function label

--- velociraptor/tools/labels.py
+++ velociraptor/tools/labels.py
@@ -14,6 +14,24 @@ def get_full_label(dataset: unyt.unyt_array):
 
     unit_tex = dataset.units.latex_representation()
 
-    full_label = f"{dataset.name} [${unit_tex}$]"
+    full_label = fr"{dataset.name} $\left[{unit_tex}\right]$"
 
     return full_label
+
+
+def get_mass_function_label(
+    mass_function_sub_label: str, mass_function_units: unyt.Unit
+):
+    """
+    Gets a fancy mass-function label such as:
+
+    d$n(M_*)$/d$\log_{10}M_*$ [Mpc$^{-3}$]
+
+    (this would be for an input of "*" and unyt.Mpc**3).
+    """
+
+    unit_repr = mass_function_units.latex_representation()
+
+    output = fr"d$n(M_{mass_function_sub_label})$/d$\log_{{10}}M_{mass_function_sub_label}$ $\left[{unit_repr}\right]$"
+
+    return output
