8346d5462a8a7ef9324b318a3a157d231575c24d
Josh Borrow <joshua.borrow@durham.ac.uk>
2019-12-11 14:20:05 +0000

    Revved version number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="velociraptor",
-    version="0.1.0",
+    version="0.1.1",
     description="Velociraptor catalogue reading routines.",
     url="https://github.com/swiftsim/velociraptor-python",
     author="Josh Borrow",
