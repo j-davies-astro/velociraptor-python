a213182df402d91ae684339bbc8dc4b84010a606
Josh Borrow <joshua.borrow@durham.ac.uk>
2019-12-12 11:02:00 +0000

    Added velociraptor-plot script to setup.py

--- setup.py
+++ setup.py
@@ -5,18 +5,20 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="velociraptor",
-    version="0.1.1",
+    version="0.2.0",
     description="Velociraptor catalogue reading routines.",
     url="https://github.com/swiftsim/velociraptor-python",
     author="Josh Borrow",
     author_email="joshua.borrow@durham.ac.uk",
     packages=setuptools.find_packages(),
+    scripts=["velociraptor-plot"],
     long_description=long_description,
     long_description_content_type="text/markdown",
     zip_safe=False,
     classifiers=[
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
         "License :: OSI Approved :: GNU Lesser General Public License v3 or later (LGPLv3+)",
         "Operating System :: OS Independent",
     ],
