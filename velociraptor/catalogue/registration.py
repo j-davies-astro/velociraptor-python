82743a642ce94f785dd21cd09f6f832bc1af3792
Evgenii Chaikin <chaikin@strw.leidenuniv.nl>
2021-03-28 14:41:35 +0200

    Fix typo in registration of H2 and HI masses

--- velociraptor/catalogue/registration.py
+++ velociraptor/catalogue/registration.py
@@ -873,7 +873,7 @@ def registration_gas_species_masses(
 
     # Capture aperture size
     match_string = (
-        "Aperture_([a-zA-Z]*)_index_0_aperture_total_gas_([0-9]*)_kpc"
+        "Aperture_([a-zA-Z]*)_aperture_total_gas_([0-9]*)_kpc"
     )
     regex = cached_regex(match_string)
 
