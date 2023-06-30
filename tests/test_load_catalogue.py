6ccb97ad1d7efc073bb4ac3dcd48c138a5241eac
Kyle Oman <kyle.a.oman@durham.ac.uk>
2023-06-30 12:16:42 +0100

    Convert from setup.py to pyproject.toml

--- tests/test_load_catalogue.py
+++ tests/test_load_catalogue.py
@@ -3,11 +3,13 @@ Basic tests for loading a catalogue.
 """
 
 from velociraptor import load
-from helper import requires
+from .helper import requires
 
 
 @requires("cosmo_0000.properties")
-def test_basic_load_catalogue_no_crash(filename="test_data/cosmo_0000.properties",):
+def test_basic_load_catalogue_no_crash(
+    filename="test_data/cosmo_0000.properties",
+):
     catalogue = load(filename)
 
     return
